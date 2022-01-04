ac60d2f5a90b95cd2925aab8d5815ef068a24afd
drtgeorgiana <drtgeorgiana@gmail.com>
2022-01-04 17:35:12 +0200

    added one to many relationship

---
+++ HousePricePrediction/HousePricePrediction.API/HousePricePrediction.API.Houses/DB/HouseDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace HousePricePrediction.API.Houses.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace HousePricePrediction.API.Houses.DB
+{
+    public class HouseDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DafaultConnection"));
+        }
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+            modelBuilder.Entity<House>()
+                .HasOne<User>(u => u.Users)
+                .WithMany(h => h.Houses)
+                .HasForeignKey(u => u.id)
+                .OnDelete(DeleteBehavior.Cascade);
+         }
+
+        public DbSet<House>? Houses { get; set; }
+        public DbSet<User>? Users { get; set; }
+    }
+}
