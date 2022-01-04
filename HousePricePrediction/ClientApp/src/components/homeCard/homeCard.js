747ff387fa2dda2ed1e74230316de09ac57a2fe7
GeorgeMoraruOct <94384463+GeorgeMoraruOct@users.noreply.github.com>
2022-01-04 10:18:57 +0200

    Fixed bugs in home and buy/rent house

--- HousePricePrediction/ClientApp/src/components/homeCard/homeCard.js
+++ HousePricePrediction/ClientApp/src/components/homeCard/homeCard.js
@@ -1,6 +1,5 @@
 import "./homeCard.css";
 import Carousel from 'react-bootstrap/Carousel';
-import House from '../../images/house.jpeg';
 import House2 from '../../images/house2.jpeg';
 import House3 from '../../images/house3.jpeg';
 import House4 from '../../images/house4.jpeg';
@@ -12,17 +11,37 @@ function HomeCard(){
             <div className="background">
                 <Carousel>
                     <Carousel.Item>
-                       <p>Welcome to House Shop!</p>
                         <div className="photoContainer">
-                            <img src={House5}/>
-                            <img src={House2}/>
+                            <div className="backgroundText">
+                                <p>Welcome to the page!</p>
+                            </div>
+                            <img src={House2} alt="b"/>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <p>Welcome to House Shop!</p>
                         <div className="photoContainer">
-                                <img src={House3}/>
-                                <img src={House4}/>
+                            <div className="backgroundText">
+                                <div>
+                                    <p>Welcome to the page!</p>
+                                </div>
+                            </div>
+                            <img src={House3} alt="a"/>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="photoContainer">   
+                            <div className="backgroundText">
+                                <p>Welcome to the page!</p>
+                            </div>
+                            <img src={House4} alt="a"/>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>       
+                        <div className="photoContainer">
+                            <div className="backgroundText">
+                                <p>Welcome to the page!</p>
+                            </div>
+                            <img src={House5} alt="a"/>
                         </div>
                     </Carousel.Item>
                 </Carousel>
