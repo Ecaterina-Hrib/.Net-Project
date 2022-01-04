b191b15908f378370e7bfa3979208cac74c7f74b
drtgeorgiana <drtgeorgiana@gmail.com>
2022-01-04 17:34:36 +0200

    added atributes

--- HousePricePrediction/HousePricePrediction.API/HousePricePrediction.API.Houses/DB/House.cs
+++ HousePricePrediction/HousePricePrediction.API/HousePricePrediction.API.Houses/DB/House.cs
@@ -1,7 +1,53 @@
 ﻿namespace HousePricePrediction.API.Houses.DB
 {
     public class House
-    {
-        public Guid Id { get; set; }
+    { 
+        private Guid _id { get; set; }
+        private long _userID { get; set; }
+        private String? _description { get; set; }
+        private String? _title { get; set; }
+        private String? _city { get; set; }
+        private String? _country { get; set; }
+        private String? _address { get; set; }
+        private String? _area { get; set; }
+        private Double _latitude { get; set; }
+        private Double _longitude { get; set; }
+        private int _constructionYear { get; set; }
+        private int _noOfRooms { get; set; }
+        private int _floor { get; set; }
+        private int _surface { get; set; }
+        private int _landSurface { get; set; }
+        private int _noOfBathrooms { get; set; }
+        private int _houseType { get; set; }
+        private int _sellType { get; set; }
+        private Dictionary<DateTime, Double> _priceHistory { get; set; }  = new Dictionary<DateTime, Double>();
+        private Dictionary<DateTime, int> _favoriteHistory { get; set; } = new Dictionary<DateTime, int>();
+        private int _noOfFave { get; set; } = 0;
+        private Double _recommendedPrice { get; set; }
+        private Double _currentPrice { get; set; }
+        private DateTime _creationDate { get; set; }
+        private Dictionary<DateTime, int> _viewsHistory { get; set; } = new Dictionary<DateTime, int>();
+        private int _views { get; set; }
+        private List<String> _pictures { get; set; } = new List<String>();
+        private User _user;
+
+        // attributes to estimate the price (ML module)
+        private string Date { get; set; }
+        private float Price  { get; set; }
+        private float Bedrooms  { get; set; } 
+        private float Bathrooms  { get; set; }
+        private float Sqft_living  { get; set; }
+        private float Sqft_lot  { get; set; }
+        private float Floors  { get; set; }
+        private float View  { get; set; }
+        private float Condition  { get; set; }
+        private float Grade  { get; set; }
+        private float Condition  { get; set; }
+        private float Sqft_basement  { get; set; }
+        private float Yr_built  { get; set; }
+        private float Yr_renovated  { get; set; }
+        private float Zipcode  { get; set; }
+        private float Lat  { get; set; }
+        private float Long  { get; set; }
     }
 }
--- HousePricePrediction/HousePricePrediction.API/HousePricePrediction.API.Houses/Model/HouseModel.cs
+++ HousePricePrediction/HousePricePrediction.API/HousePricePrediction.API.Houses/Model/HouseModel.cs
@@ -2,6 +2,33 @@
 {
     public class HouseModel
     {
-        public Guid Id { get; set; }
+        private Guid Id { get; set; }
+        private long userID { get; set; }
+        private String? description { get; set; }
+        private String? title { get; set; }
+        private String? city { get; set; }
+        private String? country { get; set; }
+        private String? address { get; set; }
+        private String? area { get; set; }
+        private Double latitude { get; set; }
+        private Double longitude { get; set; }
+        private int constructionYear { get; set; }
+        private int noOfRooms { get; set; }
+        private int floor { get; set; }
+        private int surface { get; set; }
+        private int landSurface { get; set; }
+        private int noOfBathrooms { get; set; }
+        private int houseType { get; set; }
+        private int sellType { get; set; }
+        private Dictionary<DateTime, Double> priceHistory { get; set; }  = new Dictionary<DateTime, Double>();
+        private Dictionary<DateTime, int> favoriteHistory { get; set; } = new Dictionary<DateTime, int>();
+        private int noOfFave { get; set; } = 0;
+        private Double recommendedPrice { get; set; }
+        private Double currentPrice { get; set; }
+        private DateTime creationDate { get; set; }
+        private Dictionary<DateTime, int> viewsHistory { get; set; } = new Dictionary<DateTime, int>();
+        private int views { get; set; }
+        private ICollection<String>? pictures { get; set; }
+        // private User user;
     }
 }
