17a721c6aa6409b98107faf2422504f8b14586a5
Raluca Macovei <ralumacovei@yahoo.com>
2022-01-11 01:18:41 +0200

    Update Startup.cs

--- HousePricePrediction/HousePricePrediction/HousePricePrediction.API/Startup.cs
+++ HousePricePrediction/HousePricePrediction/HousePricePrediction.API/Startup.cs
@@ -57,12 +57,12 @@ namespace HousePricePrediction.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            // if (env.IsDevelopment())
+            // {
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI();
-            }
+            // }
 
             app.UseHttpsRedirection();
 
